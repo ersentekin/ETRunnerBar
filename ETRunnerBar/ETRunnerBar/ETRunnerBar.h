8057c1857975a0aef3eb047154dd1accdec7b50e
ersentekin <ersen.tekin@gmail.com>
2014-06-18 16:39:38 +0300

    First commit

---
+++ ETRunnerBar/ETRunnerBar/ETRunnerBar.h
@@ -0,0 +1,13 @@
+//
+//  ETRunnerBar.h
+//  ETRunnerBar
+//
+//  Created by Ersen Tekin on 18.06.2014.
+//  Copyright (c) 2014 Ersen Tekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETRunnerBar : UIView
+
+@end
---
+++ ETRunnerBar/ETRunnerBar/ETRunnerBar.m
@@ -0,0 +1,31 @@
+//
+//  ETRunnerBar.m
+//  ETRunnerBar
+//
+//  Created by Ersen Tekin on 18.06.2014.
+//  Copyright (c) 2014 Ersen Tekin. All rights reserved.
+//
+
+#import "ETRunnerBar.h"
+
+@implementation ETRunnerBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
